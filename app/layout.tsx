5ea84ee3092d8d0b61c058e7d90fb6db3e9972e1
Valdeir <valdeir904@gmail.com>
2026-06-22 07:57:17 -0300

    feat: adicionar pixel do facebook no layout principal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Lora, DM_Sans } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const lora = Lora({
@@ -58,7 +59,36 @@ export default function RootLayout({
         } as React.CSSProperties
       }
     >
+      <head>
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1352669203457671');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+      </head>
       <body className="min-h-full flex flex-col">
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1352669203457671&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <ConfigProvider>
           {children}
         </ConfigProvider>
